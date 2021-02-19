3565ad9a2e31f5933c6f6ffa1dea4a4aee66c780
Ruud Lemmers <ruud.lemmers@cgi.com>
2021-02-19 16:52:01 +0100

    72 ~ Clean up code and introduce toString

--- iec61850serversimulator/src/main/java/com/cgi/iec61850serversimulator/ActionExecutor.java
+++ iec61850serversimulator/src/main/java/com/cgi/iec61850serversimulator/ActionExecutor.java
@@ -3,57 +3,60 @@ package com.cgi.iec61850serversimulator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.beanit.openiec61850.internal.cli.ActionException;
-import com.beanit.openiec61850.internal.cli.ActionListener;
 import com.beanit.openiec61850.ServerModel;
 import com.beanit.openiec61850.ServerSap;
+import com.beanit.openiec61850.internal.cli.ActionException;
+import com.beanit.openiec61850.internal.cli.ActionListener;
 
 public class ActionExecutor implements ActionListener {
-	
-	private static final Logger logger = LoggerFactory.getLogger(ServerSimulator.class);
-	private static final String PRINT_SERVER_MODEL_KEY = "p";
-	private static final String DEVICE_SHOW_MODEL = "d";
-	public ServerSap serverSap = null;
-	public ServerModel serverModel = null;
-	public Device device = null;
-	//private static final String PRINT_SERVER_MODEL_KEY = "p";
-	//private static final String PRINT_SERVER_MODEL_KEY_DESCRIPTION = "print server's model";
 
-    public ActionExecutor(ServerSap serverSap, ServerModel serverModel, Device device) {
-		this.serverSap = serverSap;
-		this.serverModel = serverModel;
-		this.device = device;
+    private static final Logger logger = LoggerFactory.getLogger(ServerSimulator.class);
+    private static final String PRINT_SERVER_MODEL_KEY = "p";
+    private static final String DEVICE_SHOW_MODEL = "d";
+    public ServerSap serverSap = null;
+    public ServerModel serverModel = null;
+    public Device device = null;
+    // private static final String PRINT_SERVER_MODEL_KEY = "p";
+    // private static final String PRINT_SERVER_MODEL_KEY_DESCRIPTION = "print
+    // server's model";
+
+    public ActionExecutor(final ServerSap serverSap, final ServerModel serverModel, final Device device) {
+        this.serverSap = serverSap;
+        this.serverModel = serverModel;
+        this.device = device;
     }
 
-	@Override
-    public void actionCalled(String actionKey) throws ActionException {
-      try {
-        switch (actionKey) {
-          case PRINT_SERVER_MODEL_KEY:
-            logger.info("** Printing model.");
-            logger.info("Server model:" + serverModel);
-            
-            break;
-            
-          case DEVICE_SHOW_MODEL:
-        	logger.info("** Printing device.");
-        	device.displayDevice();
-            //logger.debug(serverModel.toString());
-            //logger.info("** serverModel put into log as debug information.");
-        	//device.DeviceDisplay is niet static, dus static probleem. Waarom static in actionCalled???
+    @Override
+    public void actionCalled(final String actionKey) throws ActionException {
+        try {
+            switch (actionKey) {
+            case PRINT_SERVER_MODEL_KEY:
+                logger.info("** Printing model.");
+                logger.info("Server model:" + this.serverModel);
+
+                break;
+
+            case DEVICE_SHOW_MODEL:
+                logger.info("** Printing device.");
+                logger.info(this.device.toString());
+                // device.displayDevice();
+                // logger.debug(serverModel.toString());
+                // logger.info("** serverModel put into log as debug
+                // information.");
+                // device.DeviceDisplay is niet static, dus static probleem.
+                // Waarom static in actionCalled???
+            }
+        } catch (final Exception e) {
+            throw new ActionException(e);
         }
-      } catch (Exception e) {
-        throw new ActionException(e);
-      }
     }
 
-	@Override
-	public void quit() {
-		logger.info("Shutting down application...");
-		serverSap.stop();
-		return;
-		
-	}
-	
-	
+    @Override
+    public void quit() {
+        logger.info("Shutting down application...");
+        this.serverSap.stop();
+        return;
+
+    }
+
 }
--- iec61850serversimulator/src/main/java/com/cgi/iec61850serversimulator/Device.java
+++ iec61850serversimulator/src/main/java/com/cgi/iec61850serversimulator/Device.java
@@ -12,114 +12,115 @@ import com.beanit.openiec61850.BdaInt8;
 import com.beanit.openiec61850.Fc;
 
 class Device {
-	// TODO: Split the class up into several classes
-	private static final Logger logger = LoggerFactory.getLogger(Device.class);
+    // TODO: Split the class up into several classes
+    private static final Logger logger = LoggerFactory.getLogger(Device.class);
 
-	/*
-	 * String switchType1; String switchType2; String switchType3; String
-	 * switchType4; boolean relayLight2; boolean relayLight3; boolean relayLight4;
-	 * String lightType;
-	 *
-	 * int offsetAstronomSet; int offsetAstronomRise; String sensorTransition;
-	 */
-	private Clock clock;
-	private Relay[] relays;
+    private static final String SWITCH_ROOT = "SWDeviceGenericIO/XSWC";
 
-	private ServerWrapper serverWrapper;
+    private Clock clock;
+    private Relay[] relays;
 
-	/*
-	 * String eventFilterBitmask; boolean enableEventBuffered;
-	 *
-	 * // Schedule array?
-	 *
-	 * String functionalFirmwareVer; String securityFirmwareVer;
-	 *
-	 * String ipAddressGXF; int portGXF; String ipAddressNTP; int timeSyncInterval;
-	 *
-	 * boolean enableDHCP; String ipAddressDHCP; String netmaskDHCP; String
-	 * gatewayDHCP;
-	 */
+    private ServerWrapper serverWrapper;
 
-	public void displayDevice() {
-		logger.info("** Printing device:");
-		logger.info("** Printing clock.\n");
-		this.clock.displayClock();
+    public void displayDevice() {
+        logger.info("** Printing device:");
+        logger.info("** Printing clock.\n");
+        this.clock.displayClock();
 
-		logger.info("** Printing relays.\n");
-		for (int relayNr = 0; relayNr < 4; relayNr++) {
-			this.relays[relayNr].displayRelay();
-			for (int scheduleNr = 0; scheduleNr < 50; scheduleNr++) {
-				logger.info(this.getRelay(relayNr + 1).getSchedule(scheduleNr + 1).toString());
-			}
-		}
+        logger.info("** Printing relays.\n");
+        for (int relayNr = 0; relayNr < 4; relayNr++) {
+            this.relays[relayNr].displayRelay();
+            for (int scheduleNr = 1; scheduleNr <= 50; scheduleNr++) {
+                logger.info("Schedule {}: {}", scheduleNr, this.getRelay(relayNr + 1).getSchedule(scheduleNr));
+            }
+        }
+    }
 
-	}
+    @Override
+    public String toString() {
+        final StringBuilder deviceStringBuilder = new StringBuilder();
 
-	public void initalizeDevice(final ServerWrapper serverSapWrapper) {
+        deviceStringBuilder.append("** Printing device:\n");
+        deviceStringBuilder.append("** Printing clock.\n");
+        deviceStringBuilder.append(this.clock.toString() + '\n');
 
-		this.serverWrapper = serverSapWrapper;
-		this.clock = new Clock();
-		this.clock.initializeClock(this.serverWrapper.findModelNode("SWDeviceGenericIO/CSLC.Clock", Fc.CF));
-		this.relays = new Relay[4];
+        deviceStringBuilder.append("** Printing relays.\n");
+        for (int relayNr = 0; relayNr < 4; relayNr++) {
+            this.relays[relayNr].displayRelay();
+            for (int scheduleNr = 1; scheduleNr <= 50; scheduleNr++) {
+                deviceStringBuilder.append("Schedule " + scheduleNr)
+                        .append(this.getRelay(relayNr + 1).getSchedule(scheduleNr))
+                        .append('\n');
+            }
+        }
 
-		for (int relayNr = 0; relayNr < 4; relayNr++) {
-			this.relays[relayNr] = new Relay(
-					this.serverWrapper.findModelNode("SWDeviceGenericIO/XSWC" + (relayNr + 1) + ".Pos", Fc.CO),
-					this.serverWrapper.findModelNode("SWDeviceGenericIO/XSWC" + (relayNr + 1) + ".Sche.sche1", Fc.CF));
-			this.setCtlModel(relayNr + 1, 1);
-		}
-		// CTLModel naar 1 veranderen door de ServerModel te muteren
-		// Kijk naar Ruud z'n voorbeeld bij setLightStatus!
+        return deviceStringBuilder.toString();
+    }
 
-	}
+    public void initalizeDevice(final ServerWrapper serverSapWrapper) {
 
-	public Clock getClock() {
-		return this.clock;
-	}
+        this.serverWrapper = serverSapWrapper;
+        this.clock = new Clock();
+        this.clock.initializeClock(this.serverWrapper.findModelNode("SWDeviceGenericIO/CSLC.Clock", Fc.CF));
+        this.relays = new Relay[4];
 
-	public Relay getRelay(final int index) {
-		return this.relays[index - 1];
-	}
+        for (int relayNr = 0; relayNr < 4; relayNr++) {
+            this.relays[relayNr] = new Relay(
+                    this.serverWrapper.findModelNode(SWITCH_ROOT + (relayNr + 1) + ".Pos", Fc.CO),
+                    this.serverWrapper.findModelNode(SWITCH_ROOT + (relayNr + 1) + ".Sche.sche1", Fc.CF));
+            this.enableSwitching(relayNr + 1);
+        }
+        // CTLModel naar 1 veranderen door de ServerModel te muteren
+        // Kijk naar Ruud z'n voorbeeld bij setLightStatus!
 
-	public Schedule getSchedule(final int[] relayScheduleNumbers) {
-		return this.getRelay(relayScheduleNumbers[0]).getSchedule(relayScheduleNumbers[1]);
-	}
+    }
 
-	public void setLightStatus(final int relay, final boolean status) {
-		try {
-			logger.info("Set light status for relay {} to value {}", relay, status);
+    public Clock getClock() {
+        return this.clock;
+    }
 
-			final BdaBoolean lightStatus = (BdaBoolean) this.serverWrapper
-					.findModelNode("SWDeviceGenericIO/XSWC" + relay + ".Pos.Oper.ctlVal", Fc.CO);
-			// .findModelNode("SWDeviceGenericIO/XSWC" + relay + ".Pos.stVal",
-			// Fc.ST);
-			lightStatus.setValue(status);
+    public Relay getRelay(final int index) {
+        return this.relays[index - 1];
+    }
 
-			final List<BasicDataAttribute> attributes = Arrays.asList(lightStatus);
-			this.serverWrapper.setValues(attributes);
-		} catch (final Exception e) {
-			logger.error("Find node or set value failed", e);
-		}
-	}
+    public Schedule getSchedule(final int[] relayScheduleNumbers) {
+        return this.getRelay(relayScheduleNumbers[0]).getSchedule(relayScheduleNumbers[1]);
+    }
 
-	public void setCtlModel(final int relay, final int ctlModelInt) {
-		try {
-			// Necessary to have CTLModel set to 1 in the ServerModel along with having
-			// enbOpr enabled (enabled on default) to be able to switch the status.
-			logger.info("Setting CTLModel to 1 to enable relay control");
+    public void setLightStatus(final int relay, final boolean status) {
+        try {
+            logger.info("Set light status for relay {} to value {}", relay, status);
 
-			final BdaInt8 ctlModel = (BdaInt8) this.serverWrapper
-					.findModelNode("SWDeviceGenericIO/XSWC" + relay + ".Pos.ctlModel", Fc.CF);
-			byte ctlModelByte = 01;
-			logger.info("Byte: " + ctlModelByte);
-			ctlModel.setValue((byte) 01);
-			logger.info("CTLModel: {}", ctlModelByte);
+            final BdaBoolean lightStatus = (BdaBoolean) this.serverWrapper
+                    .findModelNode(SWITCH_ROOT + relay + ".Pos.Oper.ctlVal", Fc.CO);
+            lightStatus.setValue(status);
 
-			final List<BasicDataAttribute> attributes = Arrays.asList(ctlModel);
-			this.serverWrapper.setValues(attributes);
+            final List<BasicDataAttribute> attributes = Arrays.asList(lightStatus);
+            this.serverWrapper.setValues(attributes);
+        } catch (final Exception e) {
+            logger.error("Find node or set value failed", e);
+        }
+    }
 
-		} catch (final Exception e) {
-			logger.error("Setting CTLModel failed", e);
-		}
-	}
+    public void enableSwitching(final int relay) {
+        try {
+            // Necessary to have CTLModel set to 1 in the ServerModel along with
+            // having enbOpr enabled (enabled on default) to be able to switch
+            // the status.
+            logger.info("Setting CTLModel to 1 to enable relay control");
+
+            final BdaInt8 ctlModel = (BdaInt8) this.serverWrapper.findModelNode(SWITCH_ROOT + relay + ".Pos.ctlModel",
+                    Fc.CF);
+            final byte ctlModelByte = 01;
+            logger.info("Byte: {}", ctlModelByte);
+            ctlModel.setValue((byte) 01);
+            logger.info("CTLModel: {}", ctlModelByte);
+
+            final List<BasicDataAttribute> attributes = Arrays.asList(ctlModel);
+            this.serverWrapper.setValues(attributes);
+
+        } catch (final Exception e) {
+            logger.error("Setting CTLModel failed", e);
+        }
+    }
 }
