2f8aad4caa215f4ab9407726d1e4f04b3034e479
Kevin Wijnen <58944808+kevin-wijnen@users.noreply.github.com>
2021-02-22 13:47:11 +0100

    Scheduler Test instructions

--- iec61850serversimulator/src/test/java/com/cgi/iec61850serversimulator/SchedulerTest.java
+++ iec61850serversimulator/src/test/java/com/cgi/iec61850serversimulator/SchedulerTest.java
@@ -1,6 +1,12 @@
 package com.cgi.iec61850serversimulator;
 
 class SchedulerTest {
+	// TODO:
+	// Triggeren per wijziging?
+	// TaskScheduler aanroepen
+	// ScheduledFuture aanroepen
+	//
+
 	// Baseer het op het ontwerp van feature/example-scheduling!
 	// Initializing example schedule + scheduler class
 	Scheduler scheduler = new Scheduler();
